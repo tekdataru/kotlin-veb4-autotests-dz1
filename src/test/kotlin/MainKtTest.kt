cc81200b628961b6ca381691de74873614858e69
Nayanov Konstantin <8603@mail.ru>
2022-03-30 12:38:55 +0500

    some more tests 20220330

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -70,6 +70,43 @@ class MainKtTest {
     }
 
 
+    @Test
+    fun `test getSumOfFee Mir kirilitsey`() {
+        val res = getSumOfFee(6000,1000 , "Мир" )
+        assertEquals(6000, res)
+    }
+
+    @Test
+    fun `test getSumOfFee Month lim more 7500000`() {
+        val res = getSumOfFee(6000000,7500000 , "Мир" )
+        assertEquals(6045000, res)
+    }
+
+    @Test
+    fun `test getSumOfFee sum ones Vk Pay more 1500000`() {
+        val res = getSumOfFee(7500001,1000 , "Мир" )
+        assertEquals(7556251, res)
+    }
+
+    @Test
+    fun `test getSumOfFee sum Vk Pay month 4000000`() {
+        val res = getSumOfFee(4000001,1000 , "Vk Pay" )
+        assertEquals(-1, res)
+    }
+
+    @Test
+    fun `test getSumOfFee sum Vk Pay ones more 15000000`() {
+        val res = getSumOfFee(15000001,0 , "Vk Pay" )
+        assertEquals(-1, res)
+    }
+
+    @Test
+    fun `test getSumOfFee sum Vk Pay month more 60000000`() {
+        val res = getSumOfFee(60000000,1000 , "Vk Pay" )
+        assertEquals(-1, res)
+    }
+
+
 
 
 }
